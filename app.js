3b35e21aa90f3791b13b91109c34d6fce039be16
Jim Miller <miller.james01@gmail.com>
2022-01-10 21:40:06 -0500

    Added a link to project site. Fixed server start message.

--- app.js
+++ app.js
@@ -7,22 +7,28 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
+// Serve the files in the public folder
 app.use(express.static('public'));
 
+// Serving the client page
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// Stores the user information in memory.
 var users = [];
 
+// Manage socket interaction between server and client.
 io.on('connection', (socket) => {
   io.emit('leaderboard', users.slice(0, 5));
 
+  // Set up when user gives a username.
   socket.on('username', (name) => {
     var startTime = Date.now();
     var user = { 'id': socket.id, 'name': name, 'time': startTime }
     users.push(user);
     users.sort((u1, u2) => u1['time'] - u2['time']);
+
     // send updates to the users score every second
     var scoreUpdates = setInterval(() => {
       io.to(socket.id).emit('update', 
@@ -32,12 +38,12 @@ io.on('connection', (socket) => {
     }, 1000);
   });
 
-  // send one last update and remove user from competition
+  // send one last update and remove user from competition on disconnect
   socket.on('disconnect', () => {
     var endTime = Date.now();
     io.to(socket.id).emit('update',
       'You disconnected! Final score: ' +
-      Math.round((endTime - startTime) / 1000) + 's'
+      Math.round((endTime - Date.now()) / 1000) + 's'
     );
     const newUsers = users.filter(u => u['id'] !== socket.id ); 
     users = newUsers;
@@ -51,5 +57,5 @@ var leaderboard = setInterval(() => {
 
 // start the app
 server.listen(port, () => {
-  console.log(`Waiting Rooms Server listening at http://localhost:${port}`)
+  console.log(`LTS Server listening at http://localhost:${port}`)
 });
